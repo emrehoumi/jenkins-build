93ad4964d34cd0a3a91886a985479b4c4445fd47
El Mehdi REHOUMI <mehdi_rehoumi@outlook.com>
2023-05-24 16:30:25 +0200

    feat: add mvn wrapper

--- src/main/java/ma/emrehoumi/App.java
+++ src/main/java/ma/emrehoumi/App.java
@@ -1,8 +1,7 @@
 package ma.emrehoumi;
 
 public class App {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
